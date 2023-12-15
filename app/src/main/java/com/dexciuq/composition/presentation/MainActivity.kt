59bb4e0ef545c01134773d5ad232df215bb38516
dimok <zhasulanov04@list.ru>
2023-12-15 22:21:58 +0600

    Integrate view binding

--- app/src/main/java/com/dexciuq/composition/presentation/MainActivity.kt
+++ app/src/main/java/com/dexciuq/composition/presentation/MainActivity.kt
@@ -3,10 +3,22 @@ package com.dexciuq.composition.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dexciuq.composition.R
+import com.dexciuq.composition.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding
+        get() = _binding ?: error("ActivityMainBinding is null")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 }
--- app/src/main/java/com/dexciuq/composition/presentation/WelcomeFragment.kt
+++ app/src/main/java/com/dexciuq/composition/presentation/WelcomeFragment.kt
@@ -5,15 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.dexciuq.composition.R
+import com.dexciuq.composition.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
+
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding: FragmentWelcomeBinding
+        get() = _binding ?: error("FragmentWelcomeBinding is null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    ): View {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonUnderstand.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 }
