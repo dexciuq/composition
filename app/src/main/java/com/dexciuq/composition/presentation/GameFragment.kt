b092d154cf0bdb66606427083cc4fb99f278f4cf
dimok <zhasulanov04@list.ru>
2023-12-16 23:01:39 +0600

    Remove unused import and value

--- app/src/main/java/com/dexciuq/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/dexciuq/composition/presentation/GameFragment.kt
@@ -1,11 +1,9 @@
 package com.dexciuq.composition.presentation
 
-import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -27,17 +25,6 @@ class GameFragment : Fragment() {
 
     private val viewModel: GameViewModel by lazy {
         ViewModelProvider(this, viewModelFactory)[GameViewModel::class.java]
-    }
-
-    private val tvOptions by lazy {
-        listOf(
-            binding.tvOption1,
-            binding.tvOption2,
-            binding.tvOption3,
-            binding.tvOption4,
-            binding.tvOption5,
-            binding.tvOption6
-        )
     }
 
     override fun onCreateView(
